a1feed284cf9992bfabba56ca5c028d5dbda5159
Logan Lucas <logan.lucas83@gmail.com>
2024-08-31 20:59:04 +0200

    first loop of circles

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
 
 int main() {
     // create the window
@@ -18,7 +20,27 @@ int main() {
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        // window.draw(...);
+
+        /* First circles
+        sf::CircleShape circle(30.0);
+        circle.setFillColor(sf::Color(100, 250, 50));
+        window.draw(circle);
+
+        sf::CircleShape circle2(30.0);
+        circle2.setFillColor(sf::Color(230, 20, 20));
+        circle2.setPosition(0.0, 65.0);
+        window.draw(circle2);
+        */
+
+        /* Loop circles */
+        for (size_t x = 0; x < 5; x++) {
+            for (size_t y = 0; y < 5; y++) {
+                sf::CircleShape circle(30.0);
+                circle.setFillColor(sf::Color(100, 250, 50));
+                circle.setPosition(x * 65.0, y * 65.0);
+                window.draw(circle);
+            }
+        }
 
         // end the current frame
         window.display();
